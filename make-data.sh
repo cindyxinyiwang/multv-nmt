814a42365623fe2b1507b7f3d7d8f1064cb63156
cindyxinyiwang <xwang29@nd.edu>
2018-09-18 13:08:31 -0400

    scripts

--- make-data.sh
+++ make-data.sh
@@ -9,46 +9,46 @@ DDIR=/projects/tir1/corpora/multiling-text
 IL=bel
 RL=rus
 DATA_PRE=ted
-vocab_size=8000
+vocab_size=500
 
 mkdir -p  data/"$IL"_eng
 mkdir -p  data/"$RL"_eng
 mkdir -p  data/"$IL$RL"_eng
 
-ln -s $DDIR/ted/"$IL"_eng/ted-{train,dev,test}.orig."$IL"-eng data/"$IL"_eng/
-ln -s $DDIR/ted/"$RL"_eng/ted-{train,dev,test}.orig."$RL"-eng data/"$RL"_eng/
-
-for f in data/"$IL"_eng/*.orig.*-eng  data/"$RL"_eng/*.orig.*-eng; do
-  src=`echo $f | sed 's/-eng$//g'`
-  trg=`echo $f | sed 's/\.[^\.]*$/.eng/g'`
-  echo "src=$src, trg=$trg"
-  python cut-corpus.py 0 < $f > $src
-  python cut-corpus.py 1 < $f > $trg
-done
-
-for f in data/"$IL"_eng/*.orig.{eng,$IL}  data/"$RL"_eng/*.orig.{eng,$RL}; do
-  f1=${f/orig/mtok}
-  #cat $f | perl $MDIR/scripts/tokenizer/tokenizer.perl > $f1
-  cat $f | python src/reversible_tokenize.py > $f1
-done
-
-for split in train; do
-  cat data/"$IL"_eng/$DATA_PRE-$split.orig."$IL" data/"$RL"_eng/$DATA_PRE-$split.orig."$RL"  > data/"$IL$RL"_eng/$DATA_PRE-$split.orig.$IL$RL
-  cat data/"$IL"_eng/$DATA_PRE-$split.mtok."$IL" data/"$RL"_eng/$DATA_PRE-$split.mtok."$RL"  > data/"$IL$RL"_eng/$DATA_PRE-$split.mtok.$IL$RL
-  cat data/"$IL"_eng/$DATA_PRE-$split.orig.eng data/"$RL"_eng/$DATA_PRE-$split.orig.eng  > data/"$IL$RL"_eng/$DATA_PRE-$split.orig.eng
-  cat data/"$IL"_eng/$DATA_PRE-$split.mtok.eng data/"$RL"_eng/$DATA_PRE-$split.mtok.eng  > data/"$IL$RL"_eng/$DATA_PRE-$split.mtok.eng
-done
-
-for split in dev test; do
-  cp data/"$IL"_eng/$DATA_PRE-$split.orig.$IL data/"$IL$RL"_eng/
-  cp data/"$IL"_eng/$DATA_PRE-$split.mtok.$IL data/"$IL$RL"_eng/
-  cp data/"$IL"_eng/$DATA_PRE-$split.orig.eng data/"$IL$RL"_eng/
-  cp data/"$IL"_eng/$DATA_PRE-$split.mtok.eng data/"$IL$RL"_eng/
-  cp data/"$RL"_eng/$DATA_PRE-$split.orig.$RL data/"$IL$RL"_eng/
-  cp data/"$RL"_eng/$DATA_PRE-$split.mtok.$RL data/"$IL$RL"_eng/
-  cp data/"$RL"_eng/$DATA_PRE-$split.orig.eng data/"$IL$RL"_eng/
-  cp data/"$RL"_eng/$DATA_PRE-$split.mtok.eng data/"$IL$RL"_eng/
-done
+#ln -s $DDIR/ted/"$IL"_eng/ted-{train,dev,test}.orig."$IL"-eng data/"$IL"_eng/
+#ln -s $DDIR/ted/"$RL"_eng/ted-{train,dev,test}.orig."$RL"-eng data/"$RL"_eng/
+#
+#for f in data/"$IL"_eng/*.orig.*-eng  data/"$RL"_eng/*.orig.*-eng; do
+#  src=`echo $f | sed 's/-eng$//g'`
+#  trg=`echo $f | sed 's/\.[^\.]*$/.eng/g'`
+#  echo "src=$src, trg=$trg"
+#  python cut-corpus.py 0 < $f > $src
+#  python cut-corpus.py 1 < $f > $trg
+#done
+#
+#for f in data/"$IL"_eng/*.orig.{eng,$IL}  data/"$RL"_eng/*.orig.{eng,$RL}; do
+#  f1=${f/orig/mtok}
+#  #cat $f | perl $MDIR/scripts/tokenizer/tokenizer.perl > $f1
+#  cat $f | python src/reversible_tokenize.py > $f1
+#done
+#
+#for split in train; do
+#  cat data/"$IL"_eng/$DATA_PRE-$split.orig."$IL" data/"$RL"_eng/$DATA_PRE-$split.orig."$RL"  > data/"$IL$RL"_eng/$DATA_PRE-$split.orig.$IL$RL
+#  cat data/"$IL"_eng/$DATA_PRE-$split.mtok."$IL" data/"$RL"_eng/$DATA_PRE-$split.mtok."$RL"  > data/"$IL$RL"_eng/$DATA_PRE-$split.mtok.$IL$RL
+#  cat data/"$IL"_eng/$DATA_PRE-$split.orig.eng data/"$RL"_eng/$DATA_PRE-$split.orig.eng  > data/"$IL$RL"_eng/$DATA_PRE-$split.orig.eng
+#  cat data/"$IL"_eng/$DATA_PRE-$split.mtok.eng data/"$RL"_eng/$DATA_PRE-$split.mtok.eng  > data/"$IL$RL"_eng/$DATA_PRE-$split.mtok.eng
+#done
+#
+#for split in dev test; do
+#  cp data/"$IL"_eng/$DATA_PRE-$split.orig.$IL data/"$IL$RL"_eng/
+#  cp data/"$IL"_eng/$DATA_PRE-$split.mtok.$IL data/"$IL$RL"_eng/
+#  cp data/"$IL"_eng/$DATA_PRE-$split.orig.eng data/"$IL$RL"_eng/
+#  cp data/"$IL"_eng/$DATA_PRE-$split.mtok.eng data/"$IL$RL"_eng/
+#  cp data/"$RL"_eng/$DATA_PRE-$split.orig.$RL data/"$IL$RL"_eng/
+#  cp data/"$RL"_eng/$DATA_PRE-$split.mtok.$RL data/"$IL$RL"_eng/
+#  cp data/"$RL"_eng/$DATA_PRE-$split.orig.eng data/"$IL$RL"_eng/
+#  cp data/"$RL"_eng/$DATA_PRE-$split.mtok.eng data/"$IL$RL"_eng/
+#done
 
 echo "train spm from data/'$IL$RL'_eng/$DATA_PRE-train.mtok.'$IL$RL'"
 python $UDIR/train-spm.py \
@@ -73,7 +73,7 @@ do
   python $UDIR/run-spm.py \
     --model=data/eng/spm"$vocab_size".mtok.eng.model \
     < $f \
-    > ${f/mtok/mtok.spm8000} 
+    > ${f/mtok/mtok.spm$vocab_size} 
 done
 
 for f in data/"$IL"_eng/*.mtok."$IL"; 
@@ -81,7 +81,7 @@ do
   python $UDIR/run-spm.py \
     --model=data/"$IL"_eng/spm"$vocab_size.mtok.$IL".model \
     < $f \
-    > ${f/mtok/mtok.spm8000} 
+    > ${f/mtok/mtok.spm$vocab_size} 
 done
 
 for f in data/"$RL"_eng/*.mtok."$RL"; 
@@ -89,7 +89,7 @@ do
   python $UDIR/run-spm.py \
     --model=data/"$RL"_eng/spm"$vocab_size.mtok.$RL".model \
     < $f \
-    > ${f/mtok/mtok.spm8000} 
+    > ${f/mtok/mtok.spm$vocab_size} 
 done
 
 for f in data/"$IL$RL"_eng/*.mtok.{"$IL$RL","$IL"}; 
@@ -97,12 +97,13 @@ do
   python $UDIR/run-spm.py \
     --model=data/"$IL$RL"_eng/spm"$vocab_size.mtok.$IL$RL".model \
     < $f \
-    > ${f/mtok/mtok.spm8000} 
+    > ${f/mtok/mtok.spm$vocab_size} 
 done
 
-cat data/"$IL"_eng/$DATA_PRE-train.mtok.spm8000."$IL" data/"$RL"_eng/$DATA_PRE-train.mtok.spm8000."$RL"  > data/"$IL$RL"_eng/$DATA_PRE-train.mtok.sepspm8000.$IL$RL
+cat data/"$IL"_eng/$DATA_PRE-train.mtok.spm$vocab_size."$IL" data/"$RL"_eng/$DATA_PRE-train.mtok.spm$vocab_size."$RL"  > data/"$IL$RL"_eng/$DATA_PRE-train.mtok.sepspm$vocab_size.$IL$RL
 
-for f in data/"$IL"_eng/*train.mtok.* data/"$RL"_eng/*train*.mtok.* data/"$IL$RL"_eng/$DATA_PRE-train.mtok.{eng,"$IL$RL"} data/"$IL"_eng/*train.mtok.spm8000.* data/"$RL"_eng/*train*.mtok.spm8000.* data/"$IL$RL"_eng/$DATA_PRE-train.mtok.spm8000.{eng,$IL$RL} data/"$IL$RL"_eng/$DATA_PRE-train.mtok.sepspm8000.$IL$RL; do
+#for f in data/"$IL"_eng/*train.mtok.* data/"$RL"_eng/*train*.mtok.* data/"$IL$RL"_eng/$DATA_PRE-train.mtok.{eng,"$IL$RL"} data/"$IL"_eng/*train.mtok.spm8000.* data/"$RL"_eng/*train*.mtok.spm8000.* data/"$IL$RL"_eng/$DATA_PRE-train.mtok.spm8000.{eng,$IL$RL} data/"$IL$RL"_eng/$DATA_PRE-train.mtok.sepspm8000.$IL$RL; do
+for f in data/"$IL"_eng/*train.mtok.spm$vocab_size.* data/"$RL"_eng/*train*.mtok.spm$vocab_size.* data/"$IL$RL"_eng/$DATA_PRE-train.mtok.spm$vocab_size.{eng,$IL$RL} data/"$IL$RL"_eng/$DATA_PRE-train.mtok.sepspm$vocab_size.$IL$RL; do
   echo "python $XDIR/src/get_vocab.py < $f > $f.vocab &"
   python $XDIR/src/get_vocab.py < $f > $f.vocab &
 done
--- make-eng.sh
+++ make-eng.sh
@@ -2,12 +2,12 @@
 
 TDIR=/projects/tir1/corpora/multiling-text/ted
 UDIR=/projects/tir1/users/xinyiw1/utils
-vocab_size=8000
+vocab_size=32000
 
-mkdir -p data/eng
-for split in train dev test; do
-  tail -n +2 $TDIR/__multialign/all_talks_$split.tsv | cut -f 1 | python src/reversible_tokenize.py  > data/eng/ted-$split.mtok.eng
-done
+#mkdir -p data/eng
+#for split in train dev test; do
+#  tail -n +2 $TDIR/__multialign/all_talks_$split.tsv | cut -f 1 | python src/reversible_tokenize.py  > data/eng/ted-$split.mtok.eng
+#done
 
 python $UDIR/train-spm.py \
   --input=data/eng/ted-train.mtok.eng \
