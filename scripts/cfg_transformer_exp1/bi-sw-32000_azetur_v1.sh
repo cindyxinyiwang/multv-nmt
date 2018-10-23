e9ec0085b63507f906f8fba189a47322663ed7b2
cindyxinyiwang <xwang29@nd.edu>
2018-10-23 17:11:58 -0400

    transformer script

---
+++ scripts/cfg_transformer_exp1/bi-sw-32000_azetur_v1.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+#SBATCH --gres=gpu:1
+#SBATCH --mem=12g
+##SBATCH --nodelist=compute-0-7
+#SBATCH -t 0
+
+#export PYTHONPATH="$(pwd)"
+#export CUDA_VISIBLE_DEVICES="2"
+
+python src/main.py \
+  --model_type='transformer' \
+  --clean_mem_every 5 \
+  --reset_output_dir \
+  --output_dir="outputs_transformer_exp1/bi-sw-32000_azetur_v1/" \
+  --data_path data/azetur_eng/ \
+  --train_src_file_list data/azetur_eng/ted-train.mtok.sepspm32000.azetur \
+  --train_trg_file_list  data/azetur_eng/ted-train.mtok.spm8000.eng \
+  --dev_src_file  data/aze_eng/ted-dev.mtok.spm32000.aze \
+  --dev_trg_file  data/aze_eng/ted-dev.mtok.spm8000.eng \
+  --dev_trg_ref  data/aze_eng/ted-dev.mtok.eng \
+  --src_vocab_list  data/azetur_eng/ted-train.mtok.sepspm32000.azetur.vocab \
+  --trg_vocab_list  data/azetur_eng/ted-train.mtok.spm8000.eng.vocab \
+  --d_word_vec=288 \
+  --d_model=288 \
+  --d_inner=512 \
+  --n_layers=5 \
+  --n_heads=3 \
+  --log_every=50 \
+  --eval_every=2500 \
+  --ppl_thresh=15 \
+  --merge_bpe \
+  --eval_bleu \
+  --cuda \
+  --batcher='word' \
+  --batch_size 4500 \
+  --valid_batch_size=7 \
+  --patience 10 \
+  --lr_dec 0.9 \
+  --lr 0.001 \
+  --n_warm_ups 2000 \
+  --dropout 0.3 \
+  --transformer_wdrop \
+  --init_range 0.04 \
+  --max_len 10000 \
+  --seed 0
