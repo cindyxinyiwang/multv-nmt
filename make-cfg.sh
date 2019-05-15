12969c5f7104156af06d955c0582c4b0d807c622
xwang29 <cindyxinyiwang@gmail.com>
2019-05-15 19:41:46 -0400

    dds baseline runs

--- make-cfg.sh
+++ make-cfg.sh
@@ -1,9 +1,9 @@
 
-TEMP_DIR=scripts/template/
+TEMP_DIR=scripts/dds_template/
 # change random seed and directory name as desired
-CFG_DIR=cfg_s3/
-VERSION=s3
-SEED=3
+CFG_DIR=dds_baseline/
+SEED=0
+DATA_DIR='\/home\/xinyiw\/multv-nmt\/'
 
 mkdir -p scripts/"$CFG_DIR"
 # low-resource language codes
@@ -23,8 +23,8 @@ for i in ${!ILS[*]}; do
   IL=${ILS[$i]}
   RL=${RLS[$i]}
   echo $IL
-  for f in $TEMP_DIR/bi-w-16000 $TEMP_DIR/bi-sw-joint-16000 $TEMP_DIR/bi-sw-16000  $TEMP_DIR/bi-semb-bq-o16000 ; do
-    sed "s/VERSION/$VERSION/g; s/SEED/$SEED/g; s/IL/$IL/g; s/RL/$RL/g" < $f > ${f/template/"$CFG_DIR"/}_$IL$RL.sh 
-    chmod u+x ${f/template/"$CFG_DIR"/}_$IL$RL.sh 
+  for f in $TEMP_DIR/sw-8000 ; do
+    sed "s/DATA_DIR/$DATA_DIR/g; s/SEED/$SEED/g; s/IL/$IL/g; s/RL/$RL/g" < $f > ${f/dds_template/"$CFG_DIR"/}_$IL$RL$SEED.sh 
+    chmod u+x ${f/dds_template/"$CFG_DIR"/}_$IL$RL$SEED.sh 
   done
 done
